489c5f0327f20d683f2d1988f330c00743b36012
OnerrotRamo <omar.torreno@gmail.com>
2015-12-01 12:15:35 -0800

    Added program files
    This is my first time uploading files to GitHub.  I am using the GitHub
    Windows Desktop environment.  I don't know if this is the correct way of
    uploading files to GitHub.  Maybe someone will correct me.  Enjoy!

---
+++ LotteryNumberGenerator/Form1.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Code written by: Omar Torreno
+ * Date: 12/1/2015  
+ * File: Form1.cs
+ * 
+ * Description:
+ * This program will generate 5 random numbers from 1-69 and 1 number from 1-26.
+ * This can be used to play the PowerBall lottery to choose numbers for you.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        private NumberGenerator numGen = new NumberGenerator();
+        private NumberHolder numHolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Generate 5 random numbers, sort and display on button click.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numHolder = new NumberHolder();
+            for (int i = 0; i < 5; i++)
+            {
+                int number = numGen.generateNum();
+                if (!numHolder.isDuplicate(number))
+                    numHolder.addToArray(number);
+                else
+                {
+                    i--;
+                    number = numGen.generateNum();
+                }
+            }
+
+            //Sort numbers in array.
+            Array.Sort(numHolder.NumbersArray);
+            
+            //Output
+            output();
+        }
+
+        //This will display the items in the array and generate a powerball number.
+        private void output()
+        {
+            int PBNum = numGen.generatePBNum();
+            label2.Text = numHolder.NumbersArray[0].ToString();
+            label3.Text = numHolder.NumbersArray[1].ToString();
+            label4.Text = numHolder.NumbersArray[2].ToString();
+            label5.Text = numHolder.NumbersArray[3].ToString();
+            label6.Text = numHolder.NumbersArray[4].ToString();
+            label8.Text = PBNum.ToString();
+        }
+    }
+}
---
+++ LotteryNumberGenerator/NumberGenerator.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Code written by: Omar Torreno
+ * Date: 12/1/2015  
+ * File: NumberGenerator.cs
+ * 
+ * Description:
+ * This program will generate 5 random numbers from 1-69 and 1 number from 1-26.
+ * This can be used to play the PowerBall lottery to choose numbers for you.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryNumberGenerator
+{
+    class NumberGenerator
+    {        
+        private const int MAX_NUMBER = 70;
+        private const int MIN_NUMBER = 1;
+        private const int MAX_PB_NUMBER = 27;       
+        private Random numGen = new Random();
+
+        //Generate and return a number between max and min numbers.
+        public int generateNum()
+        {
+            int number = numGen.Next(MIN_NUMBER, MAX_NUMBER);
+            return number;
+        }
+
+        //Generate and return PowerBall Number.
+        public int generatePBNum()
+        {
+            int number = numGen.Next(MIN_NUMBER, MAX_PB_NUMBER);
+            return number;
+        }
+    }
+}
